e5eb4dfc13450752cc1e4ca26dbdf7cbc0b3a4d8
Rachel Kelly <rachelkelly7755@gmail.com>
2021-05-11 09:52:41 -0400

    raw code

---
+++ yelpapp/src/components/app/index.js
@@ -0,0 +1,41 @@
+
+import './styles.scss';
+
+import {useEffect, useState} from 'react';
+
+
+const App = () => {
+  const [data, setData] = useState();
+  const url= 'https://api.yelp.com/v3/autocomplete/'
+  const initData = async () => {
+    const response = await fetch(url, {
+      headers: {
+        'Conotent-Type': "application/json",
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+
+    setData(json); 
+  };
+
+  useEffect(() => {
+   initData();
+  });
+
+  return <div className="App">
+    
+    {data && data.terms.map(autocomplete => {
+      return(
+      <div> 
+        <h3>{autocomplete.businesses}</h3>
+        </div>
+        );
+    })
+    }
+  </div>
+  ;
+};
+
+
+export default App;
