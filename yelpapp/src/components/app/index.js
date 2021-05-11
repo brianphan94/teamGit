e16b5e1a4c53d3e05f8744f75a130dc2afb96a89
Rachel Kelly <rachelkelly7755@gmail.com>
2021-05-11 18:14:21 -0400

    message

--- yelpapp/src/components/app/index.js
+++ yelpapp/src/components/app/index.js
@@ -5,11 +5,14 @@ import {useEffect, useState} from 'react';
 
 
 const App = () => {
+
   const [data, setData] = useState();
   const url= 'https://api.yelp.com/v3/autocomplete/'
   const initData = async () => {
+   
     const response = await fetch(url, {
       headers: {
+        Bearers: YqvoyaNvtoC8N5dA8pD2JA
         'Conotent-Type': "application/json",
       },
     });
