8c98f3ff859c11906660b97198006c427684cd21
brianphan94 <brianphan317@gmail.com>
2021-05-11 17:51:54 -0400

    updated

--- 72hour/src/components/Home.js
+++ 72hour/src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 const Home = () => {
     return (
         <div className="main">
             <div className="mainDiv">
                 <h1>HOME FOR REACT APP</h1>
+                <h2>Created by: Brian, Rachel, and Janay</h2>
+                <img src={logo} />
             </div>
         </div>
     )
